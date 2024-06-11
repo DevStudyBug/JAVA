631f407b3bb7fd25b16e3f6a9a6ae7ec094ccb6e
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-06-12 02:59:35 +0530

    Create For-eachLoop.java

---
+++ For-eachLoop.java
@@ -0,0 +1,20 @@
+public class For-eachLoop
+{
+  public static void main(String[] args) {
+       int [] age = { 1,5,10,14,20};
+       // using loop 
+       System.out.println("for-each loop ");
+       // syntax  for (dataType:array)
+       for ( int number:age){
+       System.out.println(number);
+  	   }
+    }
+}
+
+//output 
+/*For-eachLoop
+1
+5
+10
+14
+20 */
