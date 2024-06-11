7780d3621390e1bb895a6b6d7b5707c67b7fda49
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-06-12 02:54:58 +0530

    Create LoopingArray.java
    looping through Array Element

---
+++ LoopingArray.java
@@ -0,0 +1,26 @@
+// java looping through Array Elements 
+public class LoopingArray
+{
+	public static void main(String[] args) {
+       int [] age = { 1,5,10,14,20};
+       // using loop 
+       System.out.println("Using loop printing element");
+       for ( int i =0; i< age.length;i++)
+       System.out.println("Value of array element at "+ i+ " index is: "+age[i]);
+
+	}
+}
+
+// printing array elememnt using SOP 
+/*public class Array
+{
+	public static void main(String[] args) {
+       int [] age = { 1,5,10,14,20};
+       System.out.println("array element "+age[0]);
+       System.out.println("array element "+age[1]);
+       System.out.println("array element "+age[2]);
+       System.out.println("array element "+age[3]);
+       System.out.println("array element "+age[4]);
+       System.out.println("array element "+age[5]);
+	}
+} */
