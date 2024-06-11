dd47e3b4c75d8f044eca4a99dec10638884cb203
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-06-12 03:23:24 +0530

    Create AverageofNum.java

---
+++ AverageofNum.java
@@ -0,0 +1,15 @@
+public class AverageofNum
+{
+	public static void main(String[] args) {
+       int [] numbers = { 1,5,10,14,20};
+       int sum=0;
+       double avg;
+       System.out.println("calculate sum using for-each loop ");
+       for ( int number:numbers){
+           sum+=number;   
+        }
+       avg = sum/(numbers.length);
+       System.out.println("Value of Sum is : "+ sum);
+       System.out.println("Value of Average is : "+ avg);
+    }
+}
