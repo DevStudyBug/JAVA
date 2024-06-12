2f76302b0647e6407b88e656823f0bff75647f5e
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-06-13 01:45:16 +0530

    Create Array4.java

---
+++ Array4.java
@@ -0,0 +1,13 @@
+// 4.copying Array using copyOfRange() method
+// Arrays.copyOfRange(sourceArray, int from, int to);
+
+import java.util.Arrays;
+class Array4 {
+    public static void main (String[] args){
+        int[] source = {2,3,12,4,12,-2}; 
+        int[] destination1 = Arrays.copyOfRange(source , 0,source.length);
+        System.out.println("destination1 = "+ Arrays.toString(destination1));
+        int[] destination2 = Arrays.copyOfRange(source ,2,5);
+        System.out.println("destination2 = "+ Arrays.toString(destination2));
+        }
+    }
