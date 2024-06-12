fe99b06df83aadd6d522fdc59542e518fac21eb0
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-06-12 16:11:32 +0530

    Update 2D_Array so the o/p return in tabular form

--- 2D_Array.java
+++ 2D_Array.java
@@ -1,5 +1,5 @@
 // PRINT ALL THE ELEMENT OF 2D ARRAY
-public class 2D_Array
+public class Main
 {
 	public static void main(String[] args) {
 	    int [][] a={
@@ -8,11 +8,14 @@ public class 2D_Array
 	        {7},
 	    };
 	    for (int i =0;i<a.length;i++){
-	    	    for (int j =0;j<a[i].length;j++) 
+	    	    for (int j =0;j<a[i].length;j++) {
 	    	    //a[0].length=3,a[1].length=4, a[2].length=1
 	    	    // when i=0,j=0<3 so it will print a[0][0] 
-                System.out.println(a[i][j]);
+                System.out.print(a[i][j] +" ");
+	    	    }
+	    System.out.println(" ");
 	    }
-	   // System.out.println(a.length+"length"); see the index 0,1,2 i.e
-	}
-}
+       }
+ }
+
+ // System.out.println(a.length+"length"); see the index 0,1,2 i.e=3
