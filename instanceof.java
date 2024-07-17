39afd2f832d8e1ab976e8c2e62434904e44bad34
Anshika <150266045+DevStudyBug@users.noreply.github.com>
2024-07-18 01:04:34 +0530

    Create instanceof.java

---
+++ instanceof.java
@@ -0,0 +1,15 @@
+//instanceof
+public class Main
+{
+	public static void main(String[] args) {
+	    Main obj = new Main();
+	    boolean result = obj instanceof Main;
+		System.out.println("obj is instanceof class Main "+ result);
+	
+	    String str = "java";
+	    
+	    	System.out.println(str instanceof String);
+	}
+}
+// Syntax
+// object instanceof ClassName
