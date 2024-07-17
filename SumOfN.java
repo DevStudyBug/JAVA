ef160134d172301c3bf4a052382308f07839cfa8
Anshika <150266045+DevStudyBug@users.noreply.github.com>
2024-07-17 20:48:22 +0530

    Sum of Whole numbers

---
+++ SumOfN.java
@@ -0,0 +1,22 @@
+// sum of whole number using input
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   // int n=5; 
+	    Scanner Input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int n = Input.nextInt();
+	    int sum=fun(n); 
+	    System.out.println("sum is "+sum);
+	    Input.close();
+	}
+	public static int fun (int n){
+	     if (n==1){
+	       return 1;
+	     }
+    	else{
+	       return n+ fun(n-1);
+      }
+	 }
+}
